9a406e2f1249a1b94cea413fba9101e2b36368bf
Kristiyan Ivanov <kristiyan.ivanov@redis.com>
2025-08-07 12:51:31 +0300

    fix the SSO logo image size (#4821)

--- redisinsight/ui/src/components/oauth/oauth-sign-in-button/OAuthSignInButton.tsx
+++ redisinsight/ui/src/components/oauth/oauth-sign-in-button/OAuthSignInButton.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 import { OAuthSsoHandlerDialog } from 'uiSrc/components'
 
 import RedisLogo from 'uiSrc/assets/img/logo_small.svg'
@@ -7,6 +8,11 @@ import { OAuthSocialAction, OAuthSocialSource } from 'uiSrc/slices/interfaces'
 import { SecondaryButton } from 'uiSrc/components/base/forms/buttons'
 import { RiImage } from 'uiSrc/components/base/display'
 import styles from './styles.module.scss'
+
+const LogoWrapper = styled.div`
+  width: 15px;
+  height: 15px;
+`
 
 export interface Props {
   source: OAuthSocialSource
@@ -29,7 +35,9 @@ const OAuthSignInButton = (props: Props) => {
           }
           data-testid="cloud-sign-in-btn"
         >
-          <RiImage className={styles.logo} src={RedisLogo} alt="Redis logo" />
+          <LogoWrapper>
+            <RiImage $size={"fullWidth"} className={styles.logo} src={RedisLogo} alt="Redis logo" />
+          </LogoWrapper>
           <span>Cloud sign in</span>
         </SecondaryButton>
       )}
