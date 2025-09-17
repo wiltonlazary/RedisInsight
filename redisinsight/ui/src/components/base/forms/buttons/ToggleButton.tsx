700cb08c6fad1b4be0f5f2a65a942b9476bb56b2
ArtemHoruzhenko <artsiom.kharuzhenka@gmail.com>
2025-09-17 09:13:07 +0300

    RI-7431: fix review comments

--- redisinsight/ui/src/components/base/forms/buttons/ToggleButton.tsx
+++ redisinsight/ui/src/components/base/forms/buttons/ToggleButton.tsx
@@ -1,8 +1,1 @@
-import React from 'react'
-import { ToggleButton as RedisUiToggleButton } from '@redis-ui/components'
-
-export type ButtonProps = React.ComponentProps<typeof RedisUiToggleButton>
-
-export const ToggleButton = ({ ...props }: ButtonProps) => {
-  return <RedisUiToggleButton {...props} />
-}
+export { ToggleButton } from '@redis-ui/components'
