f0053c1a9c221badcb1a7f897c7ec7fc6fb0e9ba
ArtemHoruzhenko <artsiom.kharuzhenka@gmail.com>
2025-09-17 09:09:19 +0300

    RI-7431: Delete key popover layout

--- redisinsight/ui/src/pages/browser/components/delete-key-popover/DeleteKeyPopover.tsx
+++ redisinsight/ui/src/pages/browser/components/delete-key-popover/DeleteKeyPopover.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import cx from 'classnames'
 import { KeyTypes, ModulesKeyTypes } from 'uiSrc/constants'
 import { formatLongName } from 'uiSrc/utils'
 import { RedisResponseBuffer } from 'uiSrc/slices/interfaces'
-import { Spacer } from 'uiSrc/components/base/layout/spacer'
 import {
   DestructiveButton,
   IconButton,
 } from 'uiSrc/components/base/forms/buttons'
 import { DeleteIcon } from 'uiSrc/components/base/icons'
-import { Text } from 'uiSrc/components/base/text'
+import { Text, Title } from 'uiSrc/components/base/text'
 import { RiPopover } from 'uiSrc/components/base'
+import { Col, Row } from 'uiSrc/components/base/layout/flex'
 
 export interface DeleteProps {
   nameString: string
@@ -24,6 +25,11 @@ export interface DeleteProps {
   onOpenPopover: (index: number, type: KeyTypes | ModulesKeyTypes) => void
 }
 
+const PopoverContentWrapper = styled(Col)`
+  word-break: break-all;
+  max-width: 300px;
+`
+
 export const DeleteKeyPopover = ({
   nameString,
   name,
@@ -33,40 +39,46 @@ export const DeleteKeyPopover = ({
   deleting,
   onDelete,
   onOpenPopover,
-}: DeleteProps) => (
-  <RiPopover
-    anchorClassName={cx('showOnHoverKey', { show: deletePopoverId === rowId })}
-    anchorPosition="leftUp"
-    isOpen={deletePopoverId === rowId}
-    closePopover={() => onOpenPopover(-1, type)}
-    panelPaddingSize="l"
-    button={
-      <IconButton
-        icon={DeleteIcon}
-        onClick={() => onOpenPopover(rowId, type)}
-        aria-label="Delete Key"
-        data-testid={`delete-key-btn-${nameString}`}
-      />
-    }
-    onClick={(e) => e.stopPropagation()}
-  >
-    <>
-      <Text size="m" component="div">
-        <h4 style={{ wordBreak: 'break-all' }}>
-          <b>{formatLongName(nameString)}</b>
-        </h4>
-        <Text size="s">will be deleted.</Text>
-      </Text>
-      <Spacer size="m" />
-      <DestructiveButton
-        size="small"
-        icon={DeleteIcon}
-        disabled={deleting}
-        onClick={() => onDelete(name)}
-        data-testid="submit-delete-key"
-      >
-        Delete
-      </DestructiveButton>
-    </>
-  </RiPopover>
-)
+}: DeleteProps) => {
+  const onClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.stopPropagation()
+    onOpenPopover(rowId, type)
+  }
+
+  return (
+    <RiPopover
+      anchorClassName={cx('showOnHoverKey', {
+        show: deletePopoverId === rowId,
+      })}
+      anchorPosition="leftCenter"
+      isOpen={deletePopoverId === rowId}
+      closePopover={() => onOpenPopover(-1, type)}
+      panelPaddingSize="l"
+      button={
+        <IconButton
+          icon={DeleteIcon}
+          onClick={onClick}
+          aria-label="Delete Key"
+          data-testid={`delete-key-btn-${nameString}`}
+        />
+      }
+      onClick={(e) => e.stopPropagation()}
+    >
+      <PopoverContentWrapper gap="l">
+        <Title size="S">{formatLongName(nameString)}</Title>
+        <Text size="m">will be deleted.</Text>
+        <Row>
+          <DestructiveButton
+            size="small"
+            icon={DeleteIcon}
+            disabled={deleting}
+            onClick={() => onDelete(name)}
+            data-testid="submit-delete-key"
+          >
+            Delete
+          </DestructiveButton>
+        </Row>
+      </PopoverContentWrapper>
+    </RiPopover>
+  )
+}
