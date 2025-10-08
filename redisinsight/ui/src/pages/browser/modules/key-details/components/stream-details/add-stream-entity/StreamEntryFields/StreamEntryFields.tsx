7c0e3ab023896cb631dbecc75741f0230715e30d
Artsiom Kharuzhenka <artem.horuzhenko@redis.com>
2025-10-08 11:58:35 +0300

    RI-7618: make entry id input looks the same as other inputs on the form (#5045)

--- redisinsight/ui/src/pages/browser/modules/key-details/components/stream-details/add-stream-entity/StreamEntryFields/StreamEntryFields.tsx
+++ redisinsight/ui/src/pages/browser/modules/key-details/components/stream-details/add-stream-entity/StreamEntryFields/StreamEntryFields.tsx
@@ -149,7 +149,6 @@ const StreamEntryFields = (props: Props) => {
             onChange={handleEntryIdChange}
             onBlur={onEntryIdBlur}
             onFocus={() => setIsEntryIdFocused(true)}
-            valid={!entryIdError}
             autoComplete="off"
             data-testid={config.entryId.id}
           />
