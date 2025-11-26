88bb48c35bd284cef651bfa8911d6b6cbb651a73
pd-redis <petar.dzhambazov@redis.com>
2025-11-26 17:19:41 +0200

    RI-7826: RDI data streams statistics table components and configuration (#5245)

--- redisinsight/ui/src/pages/rdi/statistics/data-streams/DataStreams.tsx
+++ redisinsight/ui/src/pages/rdi/statistics/data-streams/DataStreams.tsx
@@ -1,104 +1,20 @@
 import React from 'react'
 
 import { IDataStreams } from 'uiSrc/slices/interfaces'
-import { formatLongName } from 'uiSrc/utils'
-import { FormatedDate, RiTooltip } from 'uiSrc/components'
-import { Table, ColumnDefinition } from 'uiSrc/components/base/layout/table'
+import { Table } from 'uiSrc/components/base/layout/table'
 import { Section } from '@redis-ui/components'
 import {
   StyledRdiAnalyticsTable,
   StyledRdiStatisticsSectionBody,
 } from 'uiSrc/pages/rdi/statistics/styles'
-
-type DataStreamsData = {
-  name: string
-  total: number
-  pending: number
-  inserted: number
-  updated: number
-  deleted: number
-  filtered: number
-  rejected: number
-  deduplicated: number
-  lastArrival?: string
-}
+import { DataStreamsData } from './DataStreams.types'
+import columns from './DataStreams.config'
 
 interface Props {
   data: IDataStreams
 }
 
-const columns: ColumnDefinition<DataStreamsData>[] = [
-  {
-    header: 'Stream name',
-    id: 'name',
-    accessorKey: 'name',
-    enableSorting: true,
-    cell: ({ getValue }) => (
-      <RiTooltip content={getValue<string>()}>
-        <span>{formatLongName(getValue<string>(), 30, 0, '...')}</span>
-      </RiTooltip>
-    ),
-  },
-  {
-    header: 'Total',
-    id: 'total',
-    accessorKey: 'total',
-    enableSorting: true,
-  },
-  {
-    header: 'Pending',
-    id: 'pending',
-    accessorKey: 'pending',
-    enableSorting: true,
-  },
-  {
-    header: 'Inserted',
-    id: 'inserted',
-    accessorKey: 'inserted',
-    enableSorting: true,
-  },
-  {
-    header: 'Updated',
-    id: 'updated',
-    accessorKey: 'updated',
-    enableSorting: true,
-  },
-  {
-    header: 'Deleted',
-    id: 'deleted',
-    accessorKey: 'deleted',
-    enableSorting: true,
-  },
-  {
-    header: 'Filtered',
-    id: 'filtered',
-    accessorKey: 'filtered',
-    enableSorting: true,
-  },
-  {
-    header: 'Rejected',
-    id: 'rejected',
-    accessorKey: 'rejected',
-    enableSorting: true,
-  },
-  {
-    header: 'Deduplicated',
-    id: 'deduplicated',
-    accessorKey: 'deduplicated',
-    enableSorting: true,
-  },
-  {
-    header: 'Last arrival',
-    id: 'lastArrival',
-    accessorKey: 'lastArrival',
-    enableSorting: true,
-    cell: ({ getValue }) => <FormatedDate date={getValue<string>()} />,
-  },
-]
-
-const DataStreams = ({
-  data,
-}: Props) => {
+const DataStreams = ({ data }: Props) => {
   const dataStreams: DataStreamsData[] = Object.keys(data?.streams || {}).map(
     (key) => {
       const dataStream = data.streams[key]
