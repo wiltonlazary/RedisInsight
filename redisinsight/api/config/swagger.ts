570c84be8b13f68b46996fcb2cdb2a5153564ea8
dantovska <dijana.antovska@redis.com>
2025-12-15 14:52:49 +0200

    bump version to 3.0.1 (#5329)

--- redisinsight/api/config/swagger.ts
+++ redisinsight/api/config/swagger.ts
@@ -5,7 +5,7 @@ const SWAGGER_CONFIG: Omit<OpenAPIObject, 'paths'> = {
   info: {
     title: 'Redis Insight Backend API',
     description: 'Redis Insight Backend API',
-    version: '3.0.0',
+    version: '3.0.1',
   },
   tags: [],
 };
